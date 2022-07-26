20649dfb6dae43622f86dd36fa513ba3c5c8b0b3
Samuel Attah <sammy.attah16@gmail.com>
2022-07-26 08:47:40 +0100

    creating a server with node js

---
+++ server.js
@@ -0,0 +1,102 @@
+const EventEmitter = require("events");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+
+const { logEvents } = require("./logEvents");
+class Emmiter extends EventEmitter {}
+//initializing object:
+const myEmitter = new Emmiter();
+myEmitter.on("log", (msg, fileName) => logEvents(msg, fileName));
+const PORT = process.env.PORT || 3500;
+
+const serveFile = async (filePath, contentType, response) => {
+  try {
+    const rawData = await fsPromises.readFile(
+      filePath,
+      !contentType.includes("image") ? "utf-8" : ""
+    ); //images won't be sent with 'utf-8' so we put an empty string to let the images go through
+    const data =
+      contentType === "application/json" ? JSON.parse(rawData) : rawData; //if data is .json type we need to parse it
+    response.writeHead(filePath.includes("404.html") ? 404 : 200, {
+      "Content-Type": contentType,
+    });
+    response.end(
+      contentType === "application/json" ? JSON.stringify(data) : data
+    );
+  } catch (error) {
+    console.log(error);
+    myEmitter.emit("log", `${error.name}: ${error.message}`, "errorLog.txt");
+    response.statusCode = 500; //500 is a server error if we could not read the data from the server
+    response.end();
+  }
+};
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  myEmitter.emit("log", `${req.url}\t${req.method}`, "reqLog.txt");
+
+  const extension = path.extname(req.url);
+
+  let contentType;
+
+  switch (extension) {
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".jpg":
+      contentType = "image/jpeg";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".txt":
+      contentType = "text/plain";
+      break;
+    default:
+      contentType = "text/html";
+  }
+
+  //note that the first two lines for determining filePath below, serves to handle the default statement above
+  let filePath =
+    contentType === "text/html" && req.url === "/"
+      ? path.join(__dirname, "views", "index.html")
+      : contentType === "text/html" && req.url.slice(-1) === "/"
+      ? path.join(__dirname, "views", req.url, "index.html")
+      : contentType === "text/html"
+      ? path.join(__dirname, "views", req.url)
+      : path.join(__dirname, req.url);
+
+  //the code below makes the .html extension not required in the browser
+  if (!extension && req.url.slice(-1) !== "/") filePath += ".html";
+
+  const fileExists = fs.existsSync(filePath);
+
+  if (fileExists) {
+    serveFile(filePath, contentType, res);
+  } else {
+    //401 0r 301
+    switch (path.parse(filePath).base) {
+      case "old-page.html":
+        res.writeHead(301, { Location: "/new-page.html" });
+        res.end();
+        break;
+      case "www-page.html":
+        res.writeHead(301, { Location: "/" });
+        res.end();
+        break;
+      default:
+        //serve a 404 here, because we know the file we want to serve, we write the filePath directly, we also know the contentType
+        serveFile(path.join(__dirname, "views", "404.html"), "text/html", res);
+    }
+  }
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
